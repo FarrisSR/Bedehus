0fd8c1c2b2733a0797874acacffe6519d4dcf755
runo <runo.forrisdahl@basefarm.com>
2019-02-20 19:16:34 +0100

    Added output

--- ThingSpeakClient.py
+++ ThingSpeakClient.py
@@ -7,14 +7,14 @@ __author__ = 'Runo'
 class ThingSpeakClient:
     """ A class for fetching ICAL calendar"""
 
-    def __init__(self, url,apikey):
+    def __init__(self, url, apikey):
         self.url = url
         self.apikey = apikey
         self.debug = 1
 
 
 
-def send_data(temp):
+def send_data(self,temp):
     data = urllib.urlencode({'api_key' : KEY, 'field1': temp})
     response = urllib2.urlopen(url=BASE_URL, data=data)
     #print(response.read())
