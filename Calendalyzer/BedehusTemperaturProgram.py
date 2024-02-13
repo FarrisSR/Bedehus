b9753aa2f4ed203d0455a4e52d80960031ba449e
Runo Førrisdahl <runo.forrisdahl@basefarm.com>
2024-02-13 18:45:52 +0100

    More syslog fixing

--- Calendalyzer/BedehusTemperaturProgram.py
+++ Calendalyzer/BedehusTemperaturProgram.py
@@ -10,6 +10,13 @@ from Calendalyzer.CalendarClient import CalendarClient
 
 storsalurl = 'https://calendar.google.com/calendar/ical/84ansm753q4ru2mjc9952nel7g%40group.calendar.google.com/public/basic.ics'  # type: str
 
+class HostnameFilter(logging.Filter):
+    hostname = socket.gethostname()
+
+    def filter(self, record):
+        record.hostname = self.hostname
+        return True
+
 
 class BedehusTemperaturProgram:
     """ A class for being a dataprogram for Bedehuset """
@@ -17,6 +24,7 @@ class BedehusTemperaturProgram:
     def __init__(self):
         self.configure_logging()
         self.logger = logging.getLogger(__name__)
+        self.logger.addFilter(HostnameFilter())
 
     @staticmethod
     def configure_logging():
